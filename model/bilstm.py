add613682a6be42b44b74c3ba409104ff59c65ed
hieunghia-pat <19520178@gm.uit.edu.vn>
2021-12-16 10:21:33 +0700

    added deeper BiLSTM

--- model/bilstm.py
+++ model/bilstm.py
@@ -20,3 +20,22 @@ class BiLSTM(nn.Module):
         x = self.dropout(self.proj(x))
 
         return x
+
+class DeeperBiLSTM(nn.Module):
+    def __init__(self, vocab: Vocab, embedding_dim: int, hidden_size: int, dropout: float=0.5):
+        super(DeeperBiLSTM, self).__init__()
+
+        self.embedding = Embedding(vocab, embedding_dim)
+        self.lstm_1 = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_size, batch_first=True, bidirectional=True)
+        self.lstm_2 = nn.LSTM(input_size=2*hidden_size, hidden_size=hidden_size, batch_first=True, bidirectional=True)
+        self.proj = nn.Linear(in_features=2*hidden_size, out_features=len(vocab.output_cats))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor):
+        x = self.embedding(x)
+        x, (h, c) = self.lstm_1(x)
+        _, (x, _) = self.lstm_2(x, (h, c))
+        x = torch.cat([x[0], x[1]], dim=-1)
+        x = self.dropout(self.proj(x))
+
+        return x
---
+++ train_deeper_lstm.py
@@ -0,0 +1,132 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+
+import numpy as np
+from tqdm import tqdm
+
+import config
+from data_utils.vocab import Vocab
+from model.bilstm import DeeperBiLSTM
+from data_utils.dataset import SentimentDataset
+from metric_utils.metrics import Metrics
+from metric_utils.tracker import Tracker
+
+import os
+import pickle
+
+metrics = Metrics()
+
+def run_epoch(net, loader, optimizer, tracker, train=False, prefix='', epoch=0):
+    """ Run an epoch over the given loader """
+    if train:
+        net.train()
+        tracker_class, tracker_params = tracker.MovingMeanMonitor, {'momentum': 0.99}
+    else:
+        net.eval()
+        tracker_class, tracker_params = tracker.MeanMonitor, {}
+
+    loss_tracker = tracker.track('{}_loss'.format(prefix), tracker_class(**tracker_params))
+    acc_tracker = tracker.track('{}_accuracy'.format(prefix), tracker_class(**tracker_params))
+    pre_tracker = tracker.track('{}_precision'.format(prefix), tracker_class(**tracker_params))
+    rec_tracker = tracker.track('{}_recall'.format(prefix), tracker_class(**tracker_params))
+    f1_tracker = tracker.track('{}_F1'.format(prefix), tracker_class(**tracker_params))
+
+    criterion = nn.CrossEntropyLoss().cuda()
+    pbar = tqdm(loader, desc=f"Epoch {epoch} - {prefix}")
+    for x, y in pbar:
+        x = x.cuda()
+        y = y.cuda()
+
+        y_pred = net(x)
+
+        if train:
+            optimizer.zero_grad()
+            loss = criterion(y_pred, y)
+            loss.backward()
+            optimizer.step()
+
+            loss_tracker.append(loss.item())
+            pbar.set_postfix({"loss": loss_tracker.mean.value})
+        else:
+            loss = np.array(0)
+            scores = metrics.get_scores(y_pred.argmax(dim=-1).cpu(), y.cpu())
+
+            acc_tracker.append(scores["accuracy"])
+            pre_tracker.append(scores["precision"])
+            rec_tracker.append(scores["recall"])
+            f1_tracker.append(scores["F1"])
+            pbar.set_postfix({"accuracy": acc_tracker.mean.value, "precision": pre_tracker.mean.value, 
+                                "recall": rec_tracker.mean.value, "F1": f1_tracker.mean.value})
+
+    if not train:
+        return {
+            "accuracy": acc_tracker.mean.value,
+            "precision": pre_tracker.mean.value,
+            "recall": rec_tracker.mean.value,
+            "F1": f1_tracker.mean.value
+        }
+    else:
+        return loss_tracker.mean.value
+
+
+def main():
+
+    cudnn.benchmark = True
+
+    vocab = Vocab([config.train_path, config.val_path, config.test_path], 
+                            specials=config.specials, vectors=config.word_embedding, 
+                            tokenize_level=config.tokenize_level)
+                            
+    metrics.vocab = vocab
+    
+    train_dataset = SentimentDataset(config.train_path, vocab)
+    train_loader = train_dataset.get_loader()
+    
+    val_dataset = SentimentDataset(config.val_path, vocab)
+    val_loader = val_dataset.get_loader()
+    
+    test_dataset = SentimentDataset(config.test_path, vocab)
+    test_loader = test_dataset.get_loader()
+
+    net = nn.DataParallel(DeeperBiLSTM(vocab, config.embedding_dim, config.hidden_size)).cuda()
+    optimizer = optim.Adam(net.parameters(), lr=config.learning_rate)
+
+    tracker = Tracker()
+    config_as_dict = {k: v for k, v in vars(config).items() if not k.startswith('__')}
+
+    max_f1 = 0 # for saving the best model
+    f1_test = 0
+    for e in range(config.epochs):
+        run_epoch(net, train_loader, optimizer, tracker, train=True, prefix='Training', epoch=e)
+        val_returned = run_epoch(net, val_loader, optimizer, tracker, train=False, prefix='Validation', epoch=e)
+        test_returned = run_epoch(net, test_loader, optimizer, tracker, train=False, prefix='Evaluation', epoch=e)
+
+        results = {
+            'tracker': tracker.to_dict(),
+            'config': config_as_dict,
+            'weights': net.state_dict(),
+            'eval': {
+                'accuracy': val_returned["accuracy"],
+                "precision": val_returned["precision"],
+                "recall": val_returned["recall"],
+                "f1-val": val_returned["F1"],
+                "f1-test": test_returned["F1"]
+
+            }
+        }
+    
+        torch.save(results, os.path.join(config.model_checkpoint, "model_last.pth"))
+        if val_returned["F1"] > max_f1:
+            max_f1 = val_returned["F1"]
+            f1_test = test_returned["F1"]
+            torch.save(results, os.path.join(config.model_checkpoint, "model_best.pth"))
+
+        print("+"*13)
+
+    print(f"Training finished. Best F1 score: {max_f1}. F1 score on test set: {f1_test}")
+    print("="*31)
+
+if __name__ == '__main__':
+    main()
